9f8afb175903daa6d89637ff86ea6049abbce9a8
RinkiMaurya <96375337+RinkiMaurya@users.noreply.github.com>
2022-04-01 18:26:46 +0530

    hii

--- src/components/SubHeading.js
+++ src/components/SubHeading.js
@@ -1,8 +1,8 @@
-//import { render } from "enzyme";
+import { render } from "enzyme";
 import React from "react";
 
 function SubHeading() {
-  // render();
+  render();
   return <h5>Happy to solve you doubts.</h5>;
 }
 export default SubHeading;
